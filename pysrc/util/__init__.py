3e5fedb72dc4d80487cae03fad4dff9e0fcf94be
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2014-06-06 10:19:21 +0200

    update

--- pysrc/util/__init__.py
+++ pysrc/util/__init__.py
@@ -6,7 +6,7 @@ typeD["bcop"] = ["363"]
 typeD["eg5"] = ["290", "301", "338", "349"]
 typeD["incenp"] = ["004", "049", "052"]
 typeD["empty"] = []
-typeD["marker"] = ["001"]
+#typeD["marker"] = ["001"]
 
 typeD2={}
 typeD2["scrambled"] = ["015", "026", "063", "074", "304", "315", "352", "363"]
@@ -15,4 +15,6 @@ typeD2["eg5"] = ["290", "301", "338", "349"]
 typeD2["incenp"] = ["004", "049", "052"]
 #There are no movies without siRNA: they were deleted
 #typeD2["empty"] = ["311", "322", "359", "370"]
-typeD2["marker"] = ["001"]
+
+#Also the wells with the marker are not usable
+#typeD2["marker"] = ["001"]
