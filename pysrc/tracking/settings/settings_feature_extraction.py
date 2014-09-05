720af20d4ab58bec72afd61049f8327adf5e0f20
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2014-09-05 17:48:57 +0200

    update

--- pysrc/tracking/settings/settings_feature_extraction.py
+++ pysrc/tracking/settings/settings_feature_extraction.py
@@ -1,5 +1,5 @@
 result_folder = '../resultData/features_on_films'
-outputFile = 'distances_KS_{}.pkl'
+outputFile = 'distances2_{}.pkl'
 data_folder = '/share/data20T/mitocheck/tracking_results'
 mitocheck_file = '/cbio/donnees/aschoenauer/workspace2/Xb_screen/data/mitocheck_siRNAs_target_genes_Ens72.txt'
 quality_control_file = '/cbio/donnees/aschoenauer/workspace2/Xb_screen/data/qc_export.txt'
