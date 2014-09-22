a2ea8a7218e37a4d7eab379d281414f58e952d88
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2014-09-22 12:26:22 +0200

    update

--- pysrc/tracking/trajPack/clustering.py
+++ pysrc/tracking/trajPack/clustering.py
@@ -54,7 +54,7 @@ def correct_from_Nan(arr, perMovie):
         if not perMovie:
             toDel=np.hstack((toDel, np.where(np.isnan(arr))[0]))
         arr=np.delete(arr, toDel, 0)
-
+    pdb.set_trace()
     arr=np.hstack((arr[:,:len(featuresNumeriques)+len(featuresHisto)], arr[:,-1, np.newaxis]))
     print arr.shape
     return arr, toDel
