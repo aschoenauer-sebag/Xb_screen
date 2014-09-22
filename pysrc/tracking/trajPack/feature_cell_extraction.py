726810a6d07895a946f4e0bd17a65717f05ca82e
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2014-09-22 20:05:19 +0200

    update

--- pysrc/tracking/trajPack/feature_cell_extraction.py
+++ pysrc/tracking/trajPack/feature_cell_extraction.py
@@ -248,9 +248,12 @@ def collectingDistances(filename, folder, qc_filename='../data/mapping_2014/qc_e
                         if d[param].shape[0]==len(expList):
                             used_experiments = expList
                         else:
-                            pdb.set_trace()
-                            used_experiments = expList[np.where(usable('/share/data20T/mitocheck/tracking_results', expList,
-                                                                       qc='../data/qc_export.txt',mitocheck='../data/mitocheck_siRNAs_target_genes_Ens75.txt'))]
+                            curr_usable =np.where(usable('/share/data20T/mitocheck/tracking_results', expList,
+                                                                       qc='../data/qc_export.txt',mitocheck='../data/mitocheck_siRNAs_target_genes_Ens75.txt'))[0]
+                            if curr_usable.shape[0]==l:
+                                used_experiments = expList[curr_usable]
+                            else:
+                                pdb.set_trace()
                             
                         result[param][1].extend(list(used_experiments[l]))
                         result[param][2].extend([gene for k in range(len(l))])
