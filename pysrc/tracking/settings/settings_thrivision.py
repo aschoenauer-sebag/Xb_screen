68170fca756265c1a15448b699515e9a6d01e1df
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-02-03 21:03:10 +0100

    update

--- pysrc/tracking/settings/settings_thrivision.py
+++ pysrc/tracking/settings/settings_thrivision.py
@@ -4,7 +4,7 @@ qc_file = '/cbio/donnees/aschoenauer/workspace2/Xb_screen/data/qc_export.txt'
 
 #folders
 outputFolder = '../resultData/thrivisions'
-hdf5Folder = '/share/data20T/mitocheck/results'
+hdf5Folder = '/share/data20T/mitocheck/Alice/results'
 rawDataFolder='/share/data20T/mitocheck/compressed_data'
 trackingFolder = '/share/data20T/mitocheck/tracking_results'
 #mitocheck_file = '/cbio/donnees/aschoenauer/workspace2/Xb_screen/data/mitocheck_siRNAs_target_genes_Ens75.txt'
