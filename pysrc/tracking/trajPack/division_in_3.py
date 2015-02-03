5dd7a33315eeded9e0fda51cad9200bca50a4de2
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-02-03 21:17:27 +0100

    update

--- pysrc/tracking/trajPack/division_in_3.py
+++ pysrc/tracking/trajPack/division_in_3.py
@@ -8,6 +8,61 @@ from collections import defaultdict
 from util import settings
 from optparse import OptionParser
 from util.listFileManagement import usable_MITO
+from util import jobSize, progFolder, scriptFolder, path_command, pbsArrayEnvVar, pbsErrDir, pbsOutDir
+
+def scriptTrajFeat(exp_list, baseName='thrivision'):
+    fileNumber = int(len(exp_list)/float(jobSize))+1
+    
+    head = """#!/bin/sh
+cd %s""" %progFolder
+    
+    for k in range(fileNumber):
+        for pl, w in exp_list[jobSize*k:jobSize*(k+1)]:
+            cmd = ''
+            temp_cmd = """
+        python tracking/trajPack/division_in_3.py -p %s -w %s"""
+            temp_cmd %= (
+                        pl,
+                        w
+                        )
+            cmd += temp_cmd
+    
+        # this is now written to a script file (simple text file)
+        # the script file is called ltarray<x>.sh, where x is 1, 2, 3, 4, ... and corresponds to the job index.
+        script_name = os.path.join(scriptFolder, '%s%i.sh' % (baseName, k+1))
+        script_file = file(script_name, "w")
+        script_file.write(head + cmd)
+        script_file.close()
+
+        # make the script executable (without this, the cluster node cannot call it)
+        os.system('chmod a+x %s' % script_name)
+        
+        # write the main script
+    array_script_name = '%s.sh' % os.path.join(scriptFolder, baseName)
+    main_script_file = file(array_script_name, 'w')
+    main_content = """#!/bin/sh
+%s
+#$ -o %s
+#$ -e %s
+%s$%s.sh
+""" % (path_command,
+       pbsOutDir,  
+       pbsErrDir, 
+       os.path.join(scriptFolder, baseName),
+       pbsArrayEnvVar)
+
+    main_script_file.write(main_content)
+    os.system('chmod a+x %s' % array_script_name)
+
+    # the submission commando is:
+    #sub_cmd = 'qsub -o %s -e %s -t 1-%i %s' % (self.oBatchSettings.pbsOutDir,  
+    #                                           self.oBatchSettings.pbsErrDir, 
+    #                                           jobCount, array_script_name)
+    sub_cmd = 'qsub -t 1-%i %s' % (fileNumber, array_script_name)
+
+    print 'array containing %i jobs' % fileNumber
+    print sub_cmd
+    return 1
 
 
 class thrivisionExtraction(object):
