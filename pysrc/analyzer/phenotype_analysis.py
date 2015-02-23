73c195d0bb81ca3a0c2ab702fd229521def76501
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-02-23 15:01:40 +0100

    update

--- pysrc/analyzer/phenotype_analysis.py
+++ pysrc/analyzer/phenotype_analysis.py
@@ -635,10 +635,12 @@ if __name__ == '__main__':
 
     parser.add_option("-n", dest="nn", default=0.5, type=float,
                       help="Number of neighbours for local regression")
+     parser.add_option("-v", dest="verbose", default=0, type=int,
+                      help="Verbosity level")
         
     (options, args) = parser.parse_args()
     
-    x=xbPhenoAnalysis(options.settings_file, compound=options.xenobiotic, dose=options.dose, nn=options.nn, verbose=0, localRegMeasure=True)    
+    x=xbPhenoAnalysis(options.settings_file, compound=options.xenobiotic, dose=options.dose, nn=options.nn, verbose=options.verbose, localRegMeasure=True)    
     x()
-    x=xbPhenoAnalysis(options.settings_file, compound=options.xenobiotic, dose=options.dose, nn=options.nn, verbose=0, localRegMeasure=False)
+    x=xbPhenoAnalysis(options.settings_file, compound=options.xenobiotic, dose=options.dose, nn=options.nn, verbose=options.verbose, localRegMeasure=False)
     x()
