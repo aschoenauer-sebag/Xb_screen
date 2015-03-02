109ff538d21f44512d78f9b9c1888611e47b7f48
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-03-02 19:09:50 +0100

    Update with TrackWriter to write our trajectories in CellCognition H5 file and format

---
+++ pysrc/tracking/importPack/exp.py
@@ -0,0 +1,123 @@
+'''
+To export tracking into h5 files
+'''
+from util.settings import Settings
+import os, pdb
+import cPickle as pickle
+import vigra.impex as vi
+import numpy as np
+from _collections import defaultdict
+
+def find_id(frame, object_frame_id, object_ids):
+    try:
+        object_movie_id=np.where((object_ids['time_idx']==frame)&(object_ids['obj_label_id']==object_frame_id))[0][0]
+    except IndexError:
+        print object_frame_id
+        return None
+    else:
+        return object_movie_id
+        
+class TrackWriter(object):
+    
+    TRACK_DTYPE=np.dtype([('obj_idx1', '<u4'), ('obj_idx2', '<u4')])
+    OBJECT_PATH_NAME="/sample/0/plate/{}/experiment/{}/position/1/object/{}"
+    TRACKING_PATH_NAME="/sample/0/plate/{}/experiment/{}/position/1/object/tracking"
+
+    def __init__(self, plate, well,  setting_file="tracking/settings/settings_track_writer.py", primary_channel_name = 'primary__primary3'):
+        '''
+        Well is expected as an int
+        '''
+        self.settings=Settings(setting_file)
+        self.plate = plate
+        self.well ="{:>05}".format( well)
+        self.ch5_file=os.path.join(self.settings.CH5Folder, self.plate, 'hdf5', self.settings.CH5Filename.format(self.well))
+        self.primary_channel_name= primary_channel_name
+        
+    def _getTrajectories(self):
+        try:
+            f=open(os.path.join(self.settings.trajFolder, self.plate, self.settings.trajFilename.format(self.well)))
+            dataDict=pickle.load(f); f.close()
+        except Exception as e:
+            raise e
+        else:
+            return dataDict['tracklets dictionary'][self.plate]["{}_01".format(self.well)], dataDict['connexions between tracklets'][self.plate]["{}_01".format(self.well)]
+        
+    def _getObjectIds(self):
+        path = self.OBJECT_PATH_NAME.format(self.plate, self.well, self.primary_channel_name)
+        
+        try:
+            object_ids=vi.readHDF5(self.ch5_file, path)
+        except Exception as e:
+            raise e
+        else:
+            return object_ids
+        
+    def _getTracklets(self, tracklets, object_ids):
+        '''
+        track_id_memory will store for each trajectory with which object_movie_id it started and finished.
+        As a consequence, it won't be necessary to go through object_ids again when performing the next step
+        '''
+        result=[]; track_id_memory=defaultdict(list)
+        for track in tracklets.lstTraj:
+            points = sorted(track.lstPoints.keys())
+            nextId=None
+            
+            for i,point in enumerate(points):
+                try:
+                    nextPoint = points[i+1]
+                except IndexError:
+                    #Meaning we've reached the end of track
+                    #Possible cases: either there is only one frame in this tracklet, either not
+                    if len(points)==1:
+                        currId = find_id(frame=point[0], object_frame_id=point[1], object_ids=object_ids)
+                        track_id_memory[track.id]=[currId, currId]
+                    else:
+                        track_id_memory[track.id].append(nextId)
+                    continue
+                if nextId is None:
+                    currId = find_id(frame=point[0], object_frame_id=point[1], object_ids=object_ids)
+                    track_id_memory[track.id]=[currId]
+                else:
+                    currId = nextId
+                nextId=find_id(frame=nextPoint[0], object_frame_id=nextPoint[1], object_ids=object_ids)
+                
+                result.append((currId, nextId))
+                
+        return result, track_id_memory
+    
+    def _getTrackConnexions(self, connexions):
+        for frame in connexions:
+            currConnexions = connexions[frame]
+            for beginning in currConnexions:
+                end = currConnexions[beginning]
+                
+                for track_id in beginning:
+                    for track_id2 in end:
+                        self.track_result.append((self.track_id_memory[track_id][-1], self.track_id_memory[track_id2][0]))
+                    
+        return
+    
+    def _saveResult(self):
+        self.track_result=np.array(self.track_result, dtype=self.TRACK_DTYPE)
+        path=self.TRACKING_PATH_NAME.format(self.plate, self.well)
+        try:
+            vi.writeHDF5(self.track_result, self.ch5_file, path)
+        except Exception as e:
+            raise e
+        return 1
+        
+    def __call__(self):
+        #i. read trajectories
+        tracklets, connexions = self._getTrajectories()
+        
+        #also get table of object_id in h5 file
+        object_ids= self._getObjectIds()
+        
+        #ii. get list of connexions for each track
+        self.track_result, self.track_id_memory = self._getTracklets(tracklets, object_ids)
+        pdb.set_trace()
+        #iii. Complete list of result by adding connexions between tracklets
+        self._getTrackConnexions(connexions)
+        
+        #iv. Save this in the right h5 file
+        self._saveResult()
---
+++ pysrc/tracking/settings/settings_track_writer.py
@@ -0,0 +1,5 @@
+CH5Folder="/media/lalil0u/New/projects/Xb_screen/plates__all_features_2bis"
+CH5Filename="{}_01.ch5"
+trajFolder='/media/lalil0u/New/projects/Xb_screen/dry_lab_results/track_predictions__settings2'
+trajFilename='traj_intQC_w{}_01.hdf5.pkl'
+
