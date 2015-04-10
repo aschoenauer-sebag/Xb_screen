0bdc89731195c8af75a237de7a92ecbcf017b093
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-04-10 11:23:05 +0200

    update

--- pysrc/phenotypes/phenotype_seq.py
+++ pysrc/phenotypes/phenotype_seq.py
@@ -26,7 +26,7 @@ class pheno_seq_extractor(thrivisionExtraction):
             i+=1
             pdb.set_trace()
             try:
-                f=open(os.path.join(self.settings.outputFolder,p, self.settings.outputFile.format(p[:10], w)), 'r')
+                f=open(os.path.join(self.settings.outputFolder,pl, self.settings.outputFile.format(pl[:10], w)), 'r')
                 pheno_seq_list, mask = pickle.load(f)
                 f.close()
             except:
