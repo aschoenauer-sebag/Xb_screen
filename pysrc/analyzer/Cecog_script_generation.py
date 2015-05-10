b86cee8a22ae493fedf2c6a1bda9fc8a2a1fcbb1
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-05-10 19:09:04 +0200

    update

--- pysrc/analyzer/Cecog_script_generation.py
+++ pysrc/analyzer/Cecog_script_generation.py
@@ -31,7 +31,7 @@ class BatchProcessor(object):
         
         for plate in plates:
             wells=filter(lambda x: os.path.isdir(os.path.join(self.oBatchSettings.baseInDir,plate, x)), os.listdir(os.path.join(self.oBatchSettings.baseInDir, plate)))
-            r.extend([(plate, "{:>05}_01".format(int(well.split('--')[0]))) for well in wells])
+            r.extend([(plate, "{:>05}_01".format(int(well[1:])),"{:>05}_02".format(int(well[1:]))) for well in wells])
             
         return r
 
