2b66dcb0bb70e3102ab5c48a18660c51b94bd100
Alice Schoenauer Sebag <alice.schoenauer_sebag@mines-paristech.fr>
2015-06-24 15:23:10 +0200

    Update

--- pysrc/phenotypes/phenotype_seq.py
+++ pysrc/phenotypes/phenotype_seq.py
@@ -32,10 +32,13 @@ if getpass.getuser()=='lalil0u':
 #to do jobs launch thrivision.scriptCommand(exp_list, baseName='pheno_seq', command="phenotypes/phenotype_seq.py")
 
 def collectingDistance(type_, folder='../resultData/pheno_seq/pheno_hit'):
-    
     files=sorted(filter(lambda x: type_ in x, os.listdir(folder)), key=(lambda x:x.split('.')[0][14:]))
+    if type_=='traj':
+        len_=6225
+    elif type_=='pheno':
+        len_=6214
     
-    result=np.zeros(shape=(len(files), len(files)))
+    result=np.zeros(shape=(len_, len_))
     for i,el in enumerate(files):
         try:
             f=open(os.path.join(folder, el))
