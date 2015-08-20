14c90a538dfc95334698eaf51c938a0a4e3aecb4
lalil0u <a.lalil0u@gmail.com>
2015-08-20 17:54:43 +0200

    Update

--- pysrc/phenotypes/phenotype_seq.py
+++ pysrc/phenotypes/phenotype_seq.py
@@ -580,7 +580,7 @@ Input:
     parser.add_option("-p", "--plate", dest="plate",
                       help="The plate which you are interested in")
     
-    parser.add_option("-w", "--well", dest="well",
+    parser.add_option("-w", "--well", dest="well",type=int,
                       help="The well which you are interested in")
 
     parser.add_option("-c", dest="count_only",type=int,default=0,
