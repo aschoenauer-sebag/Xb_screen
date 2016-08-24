bbf2888b4694649e96839f5264f375a594bcc747
lalil0u <a.lalil0u@gmail.com>
2016-08-24 15:18:50 +0200

    Update

--- pysrc/phenotypes/utils.py
+++ pysrc/phenotypes/utils.py
@@ -16,8 +16,9 @@ def check_h5_files_exist(rawDataFolder = '/share/data20T/mitocheck/compressed_da
             missing_h5.extend([(plate, el) for el in well_h5files])
         else:
             missing_h5.extend([(plate, el) for el in well_h5files if '{}.ch5'.format(el) not in existing_h5files])
-        if len(well_h5files)>past_len:
-            print plate, ' missing ', len(well_h5files)-past_len
-        past_len = len(well_h5files)
+        if len(missing_h5)>past_len:
+            print plate, ' missing ', len(missing_h5)-past_len
+            
+        past_len = len(missing_h5)
         
     return missing_h5 
