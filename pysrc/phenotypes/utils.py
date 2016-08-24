d9f6a16f7f9e932b280d3a8e979c20e3db2f1357
lalil0u <a.lalil0u@gmail.com>
2016-08-24 15:15:11 +0200

    Adding utils

---
+++ pysrc/phenotypes/utils.py
@@ -0,0 +1,23 @@
+import os
+
+
+def check_h5_files_exist(rawDataFolder = '/share/data20T/mitocheck/compressed_data', 
+                         h5Folder = '/share/data40T/aschoenauer/drug_screen/results_August_2016/mito_joint_classifier'):
+    
+    plates = filter(lambda x: 'Valid' not in x, os.listdir(rawDataFolder))
+    missing_h5=[]
+    past_len=0
+    
+    for plate in plates:
+        well_h5files = ['00{}_01'.format(el.split('--')[0]) for el in os.listdir(os.path.join(rawDataFolder, plate))]
+        try:
+            existing_h5files=set(os.listdir(os.path.join(h5Folder, plate, 'hdf5')))
+        except:
+            missing_h5.extend([(plate, el) for el in well_h5files])
+        else:
+            missing_h5.extend([(plate, el) for el in well_h5files if '{}.ch5'.format(el) not in existing_h5files])
+            
+        print plate, ' missing ', len(well_h5files)-past_len
+        past_len = len(well_h5files)
+        
+    return missing_h5 
